f179d34dda24b46734fef631f58c60dd02acd780
ginger <26461046+gingerchew@users.noreply.github.com>
2025-08-21 11:49:24 -0400

    A couple more package slimming measures

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,15 @@ import { $t, makeLocalName } from './utils';
 import { toDisplayString } from './directives/text';
 
 const createApp = (appObj: Record<string, any>) => {
-    if ($t(appObj) !== 'Object') throw new Error('App definition must be an object');
+    if ($t(appObj) !== 'Object' && import.meta.env.DEV) throw new Error('App definition must be an object');
     appObj.$s = toDisplayString
-    const $store = reactive(appObj);
 
     return {
         mount() {
             Object.keys(appObj).forEach(key => key[0] !== '$' &&
                 typeof appObj[key] === 'function' &&
                 key[0].toUpperCase() === key[0] &&
-                    define(makeLocalName(key, appObj.$prefix), appObj[key], $store))
+                    define(makeLocalName(key, appObj.$prefix), appObj[key], reactive(appObj)))
         },
         directive(key: string, dir: (config: Directive<HTMLElement>) => void) {
             builtInDirectives[key] = dir;
