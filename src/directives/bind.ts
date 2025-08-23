87b7dcce0d0485d8a182137601210e215a6f0dca
ginger <26461046+gingerchew@users.noreply.github.com>
2025-08-23 13:55:40 -0400

    Fixes ts error in bind directive

--- src/directives/bind.ts
+++ src/directives/bind.ts
@@ -1,6 +1,6 @@
 import type { Directive } from ".";
 // el: HTMLElement, attrName: string, value: string, $store: Record<string, any>
 export const bind = ({ arg, exp, get, $el, effect }: Directive<HTMLElement>) => {
-    const attrName = arg?.split(':')[1];
+    const attrName = arg!.split(':')[1];
     effect(() => $el.setAttribute(attrName, get(exp)));
 }
