f51a34997887131e1e7851ac12f0551c35860a6e
ginger <26461046+gingerchew@users.noreply.github.com>
2025-08-25 16:40:12 -0400

    Updates css util to not be a bound function
    Making it a curried function is a little smaller

--- src/styles.ts
+++ src/styles.ts
@@ -1,7 +1,8 @@
 import type { $Store, ReactiveElement } from "./types";
 
 // Adds stylesheets to the reactive element shadow root or to the page
-export function css(this: ReactiveElement<$Store>, css: string) {
-    const sheet = new window.CSSStyleSheet();
-    sheet.replace(css).then(sheet => (this.shadowRoot ?? document).adoptedStyleSheets.push(sheet));
-}
+export const css = (instance: ReactiveElement<$Store>) => 
+    (css: string) => 
+        (new CSSStyleSheet())
+            .replace(css)
+            .then(sheet => (instance.shadowRoot ?? document).adoptedStyleSheets.push(sheet));
